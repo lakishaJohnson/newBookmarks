25f39fa7e8211b029bd17376ddefb5b2f9d89857
Lakisha J <lakishajohnson@pursuit.org>
2023-07-23 10:54:31 -0400

    Show & Create complete

--- controllers/bookmarkController.js
+++ controllers/bookmarkController.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const bookmarks = express.Router();
-const { getAllBookmarks } = require("../queries/bookmarks");
+const {
+  getAllBookmarks,
+  getBookmark,
+  createBookmark,
+} = require("../queries/bookmarks");
+const { checkName, checkBoolean } = require("../validations/checkBookmarks.js");
 
 // INDEX
 bookmarks.get("/", async (req, res) => {
@@ -12,5 +17,25 @@ bookmarks.get("/", async (req, res) => {
   }
 });
 
+// SHOW
+bookmarks.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const bookmark = await getBookmark(id);
+  if (bookmark) {
+    res.json(bookmark);
+  } else {
+    res.status(404).json({ error: "not found" });
+  }
+});
+
+// CREATE
+bookmarks.post("/", checkName, checkBoolean, async (req, res) => {
+  try {
+    const bookmark = await createBookmark(req.body);
+    res.json(bookmark);
+  } catch (error) {
+    res.status(400).json({ error: error });
+  }
+});
+
 module.exports = bookmarks;
-//
--- queries/bookmarks.js
+++ queries/bookmarks.js
@@ -1,5 +1,6 @@
 const db = require("../db/dbConfig.js");
 
+// INDEX: ALL BOOKMARKS
 const getAllBookmarks = async () => {
   try {
     const allBookmarks = await db.any("SELECT * FROM bookmarks");
@@ -8,4 +9,29 @@ const getAllBookmarks = async () => {
     return error;
   }
 };
-module.exports = { getAllBookmarks };
+
+// SHOW: ONE Bookmark
+const getBookmark = async (id) => {
+  try {
+    // await db.one("SELECT * FROM bookmarks WHERE id=$[id]", {id: id,});
+    const oneBookmark = await db.one("SELECT * FROM bookmarks WHERE id=$1", id);
+    return oneBookmark;
+  } catch (error) {
+    return error;
+  }
+};
+
+// CREATE: A BOOKMARK
+const createBookmark = async (bookmark) => {
+  try {
+    const newBookmark = await db.one(
+      "INSERT INTO bookmarks (name, url, category, is_favorite) VALUES($1, $2, $3, $4) RETURNING *",
+      [bookmark.name, bookmark.url, bookmark.category, bookmark.is_favorite]
+    );
+    return newBookmark;
+  } catch (error) {
+    return error;
+  }
+};
+
+module.exports = { getAllBookmarks, getBookmark, createBookmark };
